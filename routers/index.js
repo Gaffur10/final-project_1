a953ffa57a6a77bdf5b7d94f22856fd30f5e700a
Gaffur10 <adegaffur@gmail.com>
2023-12-19 00:01:10 +0800

    commit

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,6 @@ const UserRouter = require("./UserRouter");
 const ReflectionRouter = require("./ReflectionRouter");
 const { authentication } = require("../middlewares/auth");
 
-router.use("/api/v1/users", UserRouter);
 router.use("/api/v1/reflections", authentication, ReflectionRouter);
+router.use("/api/v1/users", UserRouter);
 module.exports = router;
